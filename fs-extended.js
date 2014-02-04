8f9f77f09ae86b258a1f55c092f79906c2a51b07
Mark Armendariz <src@enobrev.com>
2014-02-04 04:58:59 -0800

    added flock with duplicates of file paths in /var/lock

--- fs-extended.js
+++ fs-extended.js
@@ -1,4 +1,4 @@
-    var fs      = require('fs');
+    var fs      = require('fs-ext');
     var path    = require('path');
     var util    = require('util');
     var url     = require('url');
@@ -112,44 +112,131 @@
         });
     };
 
-    exports.copyFile = function(sFromFile, sToFile, fCallback) {
-        fCallback = typeof fCallback == 'function' ? fCallback  : function() {};
+    exports._openLockFile = function(sFile, fCallback) {
+        fs.open(sFile, 'r', function(oError, iFileHandle) {
+            if (oError) {
+                fs.open(sFile, 'w+', fCallback);
+            } else {
+                fCallback(null, iFileHandle);
+            }
+        })
+    };
 
-        var sTimer = syslog.timeStart('fs-extended.copyFile');
-        if (sFromFile != sToFile) {
-            //syslog.debug({action: 'fs-extended.copyFile', from: sFromFile, to: sToFile});
-            // CANNOT use fs.rename due to partition limitations
-            var oReader = fs.createReadStream(sFromFile);
-            var oWriter = fs.createWriteStream(sToFile);
-            oReader.pipe(oWriter);
+    exports.LOCK_TYPES = {
+        READ:  'sh',
+        WRITE: 'ex'
+    };
 
-            var bCallbackCalled = false;
+    exports.locks = {};
+    exports.lock  = function(sFile, oOptions, fCallback) {
+        sFile = '/var/lock/' + sFile.replace(/^\//, '').replace(/^var\/lock\//, '');
+        oOptions.lock    = oOptions.lock !== undefined ? oOptions.lock    : exports.LOCK_TYPES.WRITE;
+        oOptions.retries = oOptions.lock !== undefined ? oOptions.retries : 0;
+        oOptions.wait    = oOptions.lock !== undefined ? oOptions.wait    : 0;
 
-            var fDone = function(oError, sToFile) {
-                if (!bCallbackCalled) {
-                    bCallbackCalled = true;
-                    fCallback(oError, sToFile);
+        exports.mkdirP(path.dirname(sFile), 0777, function(oError) { // mocking path the file being locked in /var/lock
+            fs.open(sFile, 'w+', function(oOpenError, iFileHandle) { // creating empty writable file on which we'll run flock
+                if (oOpenError) {
+                    syslog.error({action: 'fs-extended.lock.open.error', file: sFile, type: oOptions.lock, remaining: oOptions.retries, wait: oOptions.wait, error: oOpenError});
+                    fCallback(oOpenError);
+                } else {
+                    fs.flock(iFileHandle, oOptions.lock + 'nb', function(oLockError) { // generate a non-blocking lock on the file to allow javascript to handle the retry
+                        if (!oLockError) {
+                            syslog.debug({action: 'fs-extended.lock.locked', file: sFile, type: oOptions.lock});
+                            exports.locks[sFile] = iFileHandle;
+                            fCallback()
+                        } else if (oOptions.retries > 0) {
+                            syslog.warn({action: 'fs-extended.lock.retry', file: sFile, type: oOptions.lock, remaining: oOptions.retries, wait: oOptions.wait});
+                            oOptions.retries -= 1;
+
+                            setTimeout(function() {
+                                exports.lock(sFile, oOptions, fCallback);
+                            }, oOptions.wait);
+                        } else {
+                            syslog.error({action: 'fs-extended.lock.error', file: sFile, type: oOptions.lock, remaining: oOptions.retries, wait: oOptions.wait, error: oLockError});
+                            fCallback(oLockError);
+                        }
+                    });
                 }
-            };
-
-            oReader.on('error', function(oError) {
-                syslog.debug({action: 'fs-extended.copyFile.reader.error', input: sFromFile, output: sToFile, error: oError});
-                fDone(oError);
             });
+        });
+    };
 
-            oWriter.on('error', function(oError) {
-                syslog.debug({action: 'fs-extended.copyFile.writer.error', input: sFromFile, output: sToFile, error: oError});
-                fDone(oError);
-            });
+    exports.readLock  = function(sFile, oOptions, fCallback) {
+        oOptions.lock = exports.LOCK_TYPES.READ;
+        exports.lock(sFile, oOptions, fCallback);
+    };
 
-            oWriter.on('close', function() {
-                syslog.timeStop(sTimer, {output: sToFile});
-                fDone(null, sToFile);
-            });
-        } else {
-            //syslog.debug({action: 'fs-extended.copyFile.sameFile', output: sToFile});
-            fCallback(null, sToFile);
+    exports.writeLock  = function(sFile, oOptions, fCallback) {
+        oOptions.lock = exports.LOCK_TYPES.WRITE;
+        exports.lock(sFile, oOptions, fCallback);
+    };
+
+    exports.unlock = function(sFile, fCallback) {
+        if (sFile === undefined) {
+            return fCallback(new Error('No File Given'));
         }
+
+        sFile = '/var/lock/' + sFile.replace(/^\//, '').replace(/^var\/lock\//, '');
+
+        var iFileHandle = exports.locks[sFile];
+        if (iFileHandle) { // lock was created by this process, so just delete the in-memory lock and close the file
+            syslog.debug({action: 'fs-extended.unlock.unlocked', file: sFile});
+            delete exports.locks[sFile];
+            fs.close(iFileHandle, fCallback);
+        } else {
+            exports.mkdirP(path.dirname(sFile), 0777, function(oError) { // mocking path the file being locked in /var/lock
+                fs.open(sFile, 'w+', function(oOpenError, iFileHandle) { // instead of looking for the file, we'll just create it and explicitly unlock it
+                    if (oOpenError) {
+                        fCallback(oOpenError);
+                    } else {
+                        syslog.debug({action: 'fs-extended.unlock.unlocked', file: sFile});
+                        delete exports.locks[sFile];
+                        fs.flock(iFileHandle, 'un', fCallback);
+                    }
+                });
+            });
+        }
+    };
+
+    exports.copyFile = function(sFromFile, sToFile, fCallback) {
+        async.auto({
+            lockRead:  function(fAsyncCallback, oResults) { exports.readLock(sFromFile, {retries: 300, wait: 100},  fAsyncCallback)},
+            lockWrite: function(fAsyncCallback, oResults) { exports.writeLock(sToFile,  {retries: 300, wait: 100}, fAsyncCallback)},
+            copy:      ['lockRead', 'lockWrite', function(fAsyncCallback, oResults) {
+                // CANNOT use fs.rename due to partition limitations
+                var oReader = fs.createReadStream(sFromFile);
+                var oWriter = fs.createWriteStream(sToFile);
+
+                var bCallbackCalled = false;
+
+                oWriter.on('close', function() {
+                    fAsyncCallback(null, sToFile);
+                });
+
+                oReader.on('error', function(oError) {
+                    syslog.error({action: 'fs-extended.copyFile.reader.error', input: sFromFile, output: sToFile, error: oError});
+                    fAsyncCallback(oError);
+                });
+
+                oWriter.on('error', function(oError) {
+                    syslog.error({action: 'fs-extended.copyFile.writer.error', input: sFromFile, output: sToFile, error: oError});
+                    fAsyncCallback(oError);
+                });
+
+                oReader.pipe(oWriter);
+            }]
+        }, function(oError, oResults) {
+            async.each([sFromFile, sToFile], exports.unlock, function() {
+                if (oError) {
+                    syslog.error({action: 'fs-extended.copyFile.error', input: sFromFile, output: sToFile, error: oError});
+                    fCallback(oError);
+                } else {
+                    syslog.debug({action: 'fs-extended.copyFile.done', input: sFromFile, output: sToFile});
+                    fCallback(null, oResults.copy);
+                }
+            });
+        });
     };
 
     /**
@@ -203,6 +290,7 @@
         fCallback = typeof fCallback == 'function' ? fCallback  : function() {};
 
         var sTimer = syslog.timeStart('fs-extended.copyFileToHash');
+
         exports.hashFile(sFromFile, function(oError, sHash) {
             if (oError) {
                 syslog.error({action: 'fs-extended.copyFileToHash.hashFile.error', input: sFromFile, error: oError});
@@ -248,6 +336,7 @@
         fCallback = typeof fCallback == 'function' ? fCallback  : function() {};
 
         var sTimer = syslog.timeStart('fs-extended.moveFileToHash');
+
         exports.hashFile(sFromFile, function(oError, sHash) {
             if (oError) {
                 syslog.error({action: 'fs-extended.moveFileToHash.hashFile.error', input: sFromFile, error: oError});
@@ -329,18 +418,26 @@
     exports.hashFile = function(sFile, fCallback) {
         fCallback = typeof fCallback == 'function' ? fCallback  : function() {};
 
-        var sTimer = syslog.timeStart('fs-extended.hashFile');
-        exec('sha1sum ' + sFile, function(oError, sSTDOut, sSTDError) {
-            if (oError) {
-                syslog.error({action: 'fs-extended.hashFile.error', error: oError, stdErr: sSTDError});
-                fCallback(oError);
-            } else {
-                var aHash = sSTDOut.replace(/^\s+|\s+$/g, '').split(' ');
-                var sHash = aHash[0];
+        async.auto({
+            lock:           function(fAsyncCallback, oResults) { exports.readLock(sFile, {retries: 300, wait: 100}, fAsyncCallback)},
+            hash:  ['lock', function(fAsyncCallback, oResults) {
+                exec('sha1sum ' + sFile, function(oError, sSTDOut, sSTDError) {
+                    if (oError) {
+                        syslog.error({action: 'fs-extended.hashFile.error', error: oError, stdErr: sSTDError});
+                        fCallback(oError);
+                    } else {
+                        var aHash = sSTDOut.replace(/^\s+|\s+$/g, '').split(' ');
+                        var sHash = aHash[0];
 
-                syslog.timeStop(sTimer, {output: sHash});
-                fCallback(null, sHash);
-            }
+                        //syslog.timeStop(sTimer, {output: sHash});
+                        fAsyncCallback(null, sHash);
+                    }
+                });
+            }]
+        }, function(oError, oResults) {
+            exports.unlock(sFile, function() {
+                fCallback(oError, oResults.hash);
+            });
         });
     };
 
@@ -403,7 +500,6 @@
 
         var oUrl       = url.parse(sUrl);
         var sExtension = path.extname(oUrl.pathname);
-        var oSHASum    = crypto.createHash('sha1');
         var oHTTP      = http;
         var sProtocol  = oUrl.protocol;
 
@@ -414,40 +510,48 @@
         }
 
         var sTimer = syslog.timeStart('FSX.downloadFile');
-        oHTTP.get(sUrl, function(oResponse){
-            if (oResponse.statusCode == 302 && iRedirects < 10) {
-                exports.downloadFile(oResponse.headers.location, sType, fCallback, iRedirects + 1);
-            } else {
-                var sContents = '';
-
-                oResponse.setEncoding(sType);
-                oResponse.on('data', function (sChunk) {
-                    oSHASum.update(sChunk);
-                    sContents += sChunk;
+        async.auto({
+            randomName:               function(fAsyncCallback, oResults) { crypto.randomBytes(64,             fAsyncCallback);              },
+            fullPath:  ['randomName', function(fAsyncCallback, oResults) { fAsyncCallback(null, exports.getTmpSync() + oResults.randomName) }],
+            download:  ['fullPath',   function(fAsyncCallback, oResults) {
+                var oWriter     = fs.createWriteStream(oResults.fullPath, {
+                    mode:       0777,
+                    encoding:   sType
                 });
 
-                oResponse.on('error', function (oError) {
-                    syslog.error({action: 'fs-extended.downloadFile.response.error', url: sUrl, type: sType, error: e});
-                    fCallback(oError);
-                });
-
-                oResponse.on('end', function () {
-                    var sHash      = oSHASum.digest('hex');
-                    var sFinalFile = exports.getTmpSync() + sHash + sExtension;
-                    fs.writeFile(sFinalFile, sContents, sType, function(oError) {
-                        if (oError) {
-                            syslog.error({action: 'fs-extended.downloadFile.write.error', url: sUrl, type: sType, error: oError});
-                            fCallback(oError);
+                oHTTP.get(sUrl, function(oResponse){
+                    if (oResponse.statusCode == 302) {
+                        if (iRedirects < 10) {
+                            exports.downloadFile(oResponse.headers.location, sType, fCallback, iRedirects + 1);
                         } else {
-                            syslog.debug({action: 'fs-extended.downloadFile.write', url: sUrl, type: sType, file: sFinalFile});
-                            fs.chmod(sFinalFile, 0777, function() {
-                                syslog.timeStop(sTimer, {url: sUrl, type: sType});
-                                fCallback(null, sFinalFile, sHash);
-                            });
+                            fAsyncCallback(new Error('Too Many Redirects'));
                         }
-                    });
+                    } else {
+                        oWriter.on('error', function(oError) {
+                            syslog.error({action: 'fs-extended.downloadFile.writer.error', input: sUrl, output: oResults.fullPath, error: oError});
+                            fAsyncCallback(oError);
+                        });
+
+                        oWriter.on('close', function() {
+                            fAsyncCallback(null, oResults.fullPath);
+                        });
+
+                        oResponse.setEncoding(sType);
+                        oResponse.pipe(oWriter);
+                    }
                 });
+            }],
+            move:      ['download',   function(fAsyncCallback, oResults) {
+                module.exports.moveFileToHashWithExtension(oResults.download, exports.getTmpSync(), sExtension, fAsyncCallback);
+            }]
+        }, function(oError, oResults) {
+            if (oError) {
+                syslog.error({action: 'fs-extended.downloadFile.error', input: sUrl, error: oError});
+                return fCallback(oError);
             }
+
+            syslog.timeStop(sTimer, {url: sUrl, type: sType, output: oResults});
+            fCallback(null, oResults.path, oResults.hash);
         }).on('error', function(e) {
             syslog.error({action: 'fs-extended.downloadFile.request.error', url: sUrl, type: sType, error: e});
             fCallback(e);
